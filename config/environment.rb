a62e6f44d31650497569a1899be1b8b8243e6335
jessica you <jessicaxytan@gmail.com>
2021-09-06 23:39:34 -0400

    commit tree a17e56faabbfb33e53a9b241b36e736ada059d79

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,13 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+# sql = <<-SQL
+#     CREATE IF NOT EXISTS dogs (
+#         id INTEGER PRIMARY KEY,
+#         name TEXT,
+#         breed TEXT
+#     )
+# SQL
+
+# ActiveRecord::Base.connection.execute(sql)
